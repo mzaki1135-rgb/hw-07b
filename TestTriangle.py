e55612064016c3f14b76a13efd6e98305ecf5411
mzaki1135-rgb <mzaki1135@gmail.com>
2026-03-30 10:45:29 -0400

    Add additional triangle classification tests

--- TestTriangle.py
+++ TestTriangle.py
@@ -48,6 +48,35 @@ class TestTriangles(unittest.TestCase):
 
     def testNonInteger(self):
         self.assertEqual(classifyTriangle(3.5,4,5),'InvalidInput')
+            def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testScalene(self):
+        self.assertEqual(classifyTriangle(3,4,6),'Scalene')
+
+    def testIsosceles(self):
+        self.assertEqual(classifyTriangle(5,5,3),'Isoceles')
+
+    def testInvalidZero(self):
+        self.assertEqual(classifyTriangle(0,1,2),'InvalidInput')
+
+    def testInvalidNegative(self):
+        self.assertEqual(classifyTriangle(-1,2,3),'InvalidInput')
+
+    def testTooLarge(self):
+        self.assertEqual(classifyTriangle(201,2,3),'InvalidInput')
+
+    def testNotTriangle(self):
+        self.assertEqual(classifyTriangle(1,2,3),'NotATriangle')
+
+    def testRightTriangleC(self):
+        self.assertEqual(classifyTriangle(6,8,10),'Right')
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
